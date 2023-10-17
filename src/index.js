547f9f562a44cf9de897cb87f7f0d5bba8ba8cda
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-10-17 17:11:57 +0200

    feat: Configure Knex.js for MySQL database connection
    - Definieer de Knex-configuratie voor database-interactie met MySQL.
    - Maak een databaseverbinding met de opgegeven configuratie.
    - Voer een eenvoudige query uit om te controleren of de databaseverbinding werkt.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+// Importeer de express-module voor het maken van een webserver
 const express = require('express');
 const app = express();
-const port = 3000;
 
+// Definieer de poort waarop de server moet luisteren
+const port = process.env.PORT || 3000;
 
+// Importeer de knex-module voor database-interactie
+const knex = require('knex');
+const knexConfig = require('../knexfile'); // Zorg ervoor dat het juiste pad wordt opgegeven
+
+// Maak een databaseverbinding met behulp van knex
+const db = knex(knexConfig.development);
+
+// Voer een eenvoudige query uit om te controleren of de databaseverbinding werkt
+db.raw("SELECT 1=1").then((result) => {
+    console.log(result);
+});
+
+// Definieer een route voor de hoofdpagina
 app.get('/', (req, res) => {
     res.send('Welkom bij mijn project!');
 });
 
-// Start de server
+// Start de server en luister naar de opgegeven poort
 app.listen(port, (err) => {
     if (!err) {
         console.log(`De server luistert op poort ${port}`);
     } else {
-        console.error(err)
+        console.error(err);
     }
-})
+});
