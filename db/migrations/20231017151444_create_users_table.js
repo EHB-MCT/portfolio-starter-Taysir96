7b091ec3f35266afb4fad1d716439429f70e39c7
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-10-17 17:21:05 +0200

    feat: Add Knex migration for 'users' table creation
    - Voeg een Knex-migratie toe voor het aanmaken van de 'users'-tabel.
    - Definieer de structuur van de tabel, inclusief kolommen voor 'id', 'first_name', 'last_name', 'email', 'password', en 'role'.
    - Voeg commentaren toe om de structuur van de tabel te beschrijven en geef aan dat je kolommen kunt toevoegen indien nodig.
    - Voeg een down-migratie toe om de 'users'-tabel ongedaan te maken door deze te verwijderen.

---
+++ db/migrations/20231017151444_create_users_table.js
@@ -0,0 +1,25 @@
+/**
+ * Voert de migratie voor het aanmaken van de 'users'-tabel uit.
+ * @param {import("knex").Knex} knex - Het Knex-object voor database-interactie.
+ * @returns {Promise<void>} - Een Promise die wordt opgelost zodra de migratie is voltooid.
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments('id').primary();
+        table.string('first_name');
+        table.string('last_name');
+        table.string('email').unique();
+        table.string('password');
+        table.string('role');
+        // Voeg hier andere kolommen toe indien nodig
+    });
+};
+
+/**
+ * Maakt de 'users'-tabel ongedaan door deze te verwijderen.
+ * @param {import("knex").Knex} knex - Het Knex-object voor database-interactie.
+ * @returns {Promise<void>} - Een Promise die wordt opgelost zodra de migratie is voltooid.
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('users');
+};
