98706012352f7e033b6702102ca778f0e9327d5b
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-10-31 20:17:30 +0100

    feat: Add endpoints for retrieving, deleting, and updating items
    - Implementeer Express-routes voor het ophalen van alle items, verwijderen van items op basis van ID en bijwerken van items op basis van ID.
    - Voeg endpoints toe voor het ophalen van items, het verwijderen van items en het bijwerken van items met validatie.
    - Stuur passende HTTP-responsen afhankelijk van de uitkomst van elke bewerking (ophalen, verwijderen, bijwerken).

--- images/api/src/routes/itemsRoutes.js
+++ images/api/src/routes/itemsRoutes.js
@@ -29,4 +29,62 @@ router.post('/create-item', async (req, res) => {
     }
 });
 
+/**
+ * Endpoint to retrieve all items.
+ */
+router.get('/get-items', async (req, res) => {
+    try {
+        const items = await db('items');
+        res.status(200).json({ items });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while fetching the items' });
+    }
+});
+
+/**
+ * Endpoint to delete an item based on its ID.
+ */
+router.delete('/delete-item/:itemsId', async (req, res) => {
+    try {
+        const itemsId = req.params.itemsId;
+
+        // Remove the item from the database based on itemsId
+        const deletedItem = await db('items').where('itemsId', itemsId).del();
+
+        if (deletedItem) {
+            res.json({ message: 'Item has been deleted' });
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while deleting the item' });
+    }
+});
+
+/**
+ * Endpoint to update an item based on its ID.
+ */
+router.put('/update-item/:itemsId', async (req, res) => {
+    try {
+        const itemsId = req.params.itemsId;
+        const { name, description, is_available, img_link } = req.body;
+
+        // Update the item in the database based on itemsId
+        const updatedItem = await db('items')
+            .where('itemsId', itemsId)
+            .update({ name, description, is_available, img_link });
+
+        if (updatedItem) {
+            res.json({ message: 'Item has been updated' });
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while updating the item' });
+    }
+});
+
 module.exports = router;
